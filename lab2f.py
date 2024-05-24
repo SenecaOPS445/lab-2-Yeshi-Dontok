2d25032e4d0d7ca0b66ad789c44cb88e6623ca16
yeshi <yeshidhontok@gmail.com>
2024-05-24 16:47:26 -0400

    accomplished lab2f.py

---
+++ lab2f.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+#Autor: Yeshi Dhontok Lama	
+#Author ID: 131348211
+#Date Created: 2024/05/04
+
+import sys
+timer = int(sys.argv[1])
+
+while timer !=0:
+    print(timer)
+    timer = timer - 1
+
+print('blast off!') 
