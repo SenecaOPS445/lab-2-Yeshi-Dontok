eaca1a2bc73ae6a45669b3d5b02fa10cabece339
yeshi <yeshidhontok@gmail.com>
2024-05-24 16:46:28 -0400

    accomplished lab2d.py

---
+++ lab2e.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+#Autor: Yeshi Dhontok Lama	
+#Author ID: 131348211
+#Date Created: 2024/05/04
+
+timer = 10
+
+while timer !=0:
+   print(timer)
+   timer = timer - 1
+
+print('blast off!') 
